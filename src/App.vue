ef0d262d5440b833283e17c13e0fa65b2ed904e9
luojinan <luojinan@zhuanzhuan.com>
2024-04-16 19:58:58 +0800

    first init vite vue3 taillwindcss daiyui

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// const msg = ref('Vite + Vue')
+</script>
+
+<template>
+  <div class="fixed top-8 right-2 btn btn-primary">
+    ads
+  </div>
+</template>
+
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import monkey, { cdn } from 'vite-plugin-monkey';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    monkey({
+      entry: 'src/main.ts',
+      userscript: {
+        icon: 'https://vitejs.dev/logo.svg',
+        namespace: 'npm/vite-plugin-monkey',
+        match: ['https://detail.tmall.com/*','https://main.m.taobao.com/security-h5-detail/*'],
+      },
+      build: {
+        externalGlobals: {
+          vue: cdn.npmmirror('Vue', 'dist/vue.global.prod.js'),
+        },
+      },
+    }),
+  ],
+});
